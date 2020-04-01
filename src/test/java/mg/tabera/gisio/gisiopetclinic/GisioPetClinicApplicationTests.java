d0cbf2c8bdadb28c222db41eb2858e3babd76dfd
GisioW <gisio.tabera@gmail.com>
2020-04-01 22:56:02 +0200

    Add Project

---
+++ src/test/java/mg/tabera/gisio/gisiopetclinic/GisioPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.tabera.gisio.gisiopetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisioPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
